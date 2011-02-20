f1a01b6a3f840c06dfda8b9ee4d897ddd9fe6777
ryan <ryan@ofc.(none)>
2011-02-20 11:45:16 -0800

    added copy-to-home script

---
+++ html/files/local-copy/copy-to-home.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/webwork/ryana804.com/html/files/local-copy/.bash_profile ~/
+cp ~/webwork/ryana804.com/html/files/local-copy/.git-completion.sh ~/
+cp ~/webwork/ryana804.com/html/files/local-copy/.vimrc ~/
