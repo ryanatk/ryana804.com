d1d5b6cf71a92cf91c98bbd92f568da6f7527022
Ryan A <ryana804@gmail.com>
2012-03-04 03:21:07 +0000

    added bbfl pages and favorites

--- html/bbfl/js/roster-grid.js
+++ html/bbfl/js/roster-grid.js
@@ -1,2 +1,11 @@
 (function () {
+  var body = document.body;
+
+  body.addEventListener('click', function (e) {
+    var target = e.target;
+    var targetClass = target.className;
+
+    var isTeam = 0;
+  });
 })();
+
