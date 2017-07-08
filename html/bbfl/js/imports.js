cdd413fa854a0ec2d2145a060ac76e363dd45424
ryan atkinson <ryana804@gmail.com>
2017-07-08 13:04:16 -0700

    added imports, moved styles to css

---
+++ html/bbfl/js/imports.js
@@ -0,0 +1,23 @@
+var importTemplates = function () {
+  var templates = {};
+  var template, key;
+  var links = document.querySelectorAll('link[rel="import"]');
+  var len = links.length;
+
+  while (len--) {
+    // Clone the <template> in the import.
+    template = links[len].import.querySelector('template');
+    key = template.id;
+    if (key)
+      templates[key] = template;
+  }
+
+  return templates;
+};
+
+var addImport = function (selector, templateId) {
+  var template = importTemplates[templateId];
+  var clone = document.importNode(template.content, true);
+
+  document.querySelector(selector).appendChild(clone);
+};
